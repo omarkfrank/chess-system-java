fff079cca24ea93b38a03c01b16d96ceb310a690
omarkfrank <omarkfrank@yahoo.com>
2024-04-30 17:02:37 -0300

    Implementando movimentos possíveis da torre

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -22,6 +22,11 @@ public class Position {
 		this.column = colum;
 	}
 	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;		
+	}
+	
 	@Override
 	public String toString() {
 		return row + ", " + column;
