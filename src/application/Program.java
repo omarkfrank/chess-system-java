4bc66ebfc10a994e4f81d1ca1bc2be8d256a42d0
omarkfrank <omarkfrank@yahoo.com>
2024-05-08 19:12:16 -0300

    Fix - Implementado melhor tratativa para promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,7 +42,11 @@ public class Program {
 
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Entre com a Peca a ser promovida(B/N/R/Q): ");
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Valor invalido! Entre novamente com a peca a ser Promovida (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -122,7 +121,7 @@ public class ChessMatch {
 			throw new IllegalStateException("Indiponivel peca para Promover");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Tipo invalido para Promover");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
